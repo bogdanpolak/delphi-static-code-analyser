a8891c319f2e2679af724ac76657a178d282d927
Bogdan Polak <bogdanpol@gmail.com>
2020-11-22 22:54:29 +0100

    Refactored Main unit

--- src/Main.pas
+++ src/Main.pas
@@ -24,42 +24,6 @@ uses
   Analytics.Generator,
   Analytics.UnitMetrics;
 
-function Parse(const FileName: string): TUnitMetrics;
-var
-  syntaxtree: TSyntaxNode;
-  Builder: TPasSyntaxTreeBuilder;
-  StringStream: TStringStream;
-begin
-  Result := nil;
-  try
-    Builder := TPasSyntaxTreeBuilder.Create;
-    try
-      StringStream := TStringStream.Create;
-      try
-        StringStream.LoadFromFile(FileName);
-        Builder.IncludeHandler := TIncludeHandler.Create
-          (ExtractFilePath(FileName));
-        StringStream.Position := 0;
-        syntaxtree := Builder.Run(StringStream);
-        try
-          Result := TAnalyticsGenerator.Build(syntaxtree);
-          // writeln(TSyntaxTreeWriter.ToXML(syntaxtree, true));
-        finally
-          syntaxtree.Free;
-        end;
-      finally
-        StringStream.Free;
-      end;
-    finally
-      Builder.Free;
-    end
-  except
-    on E: ESyntaxTreeException do
-      writeln(Format('[%d, %d] %s', [E.Line, E.Col, E.Message]) + sLineBreak +
-        sLineBreak + TSyntaxTreeWriter.ToXML(E.syntaxtree, True));
-  end;
-end;
-
 function GetTestFolder(): string;
 begin
   if DirectoryExists('..\..\..\test\data') then
@@ -70,24 +34,81 @@ begin
     raise Exception.Create('Can''t find test data folder.');
 end;
 
+function LoadUnit(const FileName: string): TStream;
+var
+  strStream: TStringStream;
+begin
+  strStream := TStringStream.Create;
+  strStream.LoadFromFile(FileName);
+  strStream.Position := 0;
+  Result := strStream;
+end;
+
+function BuildMetrics(aUnitStream: TStream; const aIncludeFolder: string = '')
+  : TUnitMetrics;
+var
+  Builder: TPasSyntaxTreeBuilder;
+  syntaxTree: TSyntaxNode;
+begin
+  Builder := TPasSyntaxTreeBuilder.Create;
+  try
+    if aIncludeFolder <> '' then
+    begin
+      Builder.IncludeHandler := TIncludeHandler.Create(aIncludeFolder);
+    end;
+    try
+      syntaxTree := Builder.Run(aUnitStream);
+      try
+        Result := TAnalyticsGenerator.Build(syntaxTree);
+        // writeln(TSyntaxTreeWriter.ToXML(syntaxTree, true));
+      finally
+        syntaxTree.Free;
+      end;
+    except
+      on E: ESyntaxTreeException do
+      begin
+        writeln(Format('[%d, %d] %s', [E.Line, E.Col, E.Message]) + sLineBreak +
+          sLineBreak + TSyntaxTreeWriter.ToXML(E.syntaxTree, True));
+        raise;
+      end;
+    end;
+  finally
+    Builder.Free;
+  end
+end;
+
+procedure DisplayMetricsResults(aUnitMetrics: TUnitMetrics);
+var
+  idx: Integer;
+begin
+  writeln(aUnitMetrics.Name);
+  for idx := 0 to aUnitMetrics.MethodsCount - 1 do
+    writeln('  - ', aUnitMetrics.Method[idx].ToString);
+end;
+
+procedure RunUnitAnalyser(const fname: string);
+var
+  metrics: TUnitMetrics;
+  stream: TStream;
+begin
+  stream := LoadUnit(fname);
+  try
+    metrics := BuildMetrics(stream);
+    DisplayMetricsResults(metrics);
+    metrics.Free;
+  finally
+    stream.Free;
+  end;
+end;
+
 procedure ApplicationRun();
 var
   fname: string;
-  unitmetrics: TUnitMetrics;
-  idx: Integer;
 begin
-  writeln('DelphiAST Console Writer Demo');
+  writeln('DelphiAST - Static Code Analyser');
   writeln('----------------------------------');
   fname := TPath.Combine(GetTestFolder, 'testunit.pas');
-  unitmetrics := Parse(fname);
-  try
-    writeln(fname);
-    for idx := 0 to unitmetrics.MethodsCount-1 do
-      writeln('  - ',unitmetrics.Method[idx].ToString);
-  finally
-    if unitmetrics<>nil then
-      unitmetrics.Free;
-  end;
+  RunUnitAnalyser(fname);
   readln;
 end;
 
