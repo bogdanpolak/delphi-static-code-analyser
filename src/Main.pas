9492ac5165414e9887d953c821636710c1f3a192
Bogdan Polak <bogdanpol@gmail.com>
2020-11-25 23:18:34 +0100

    Added app configuration

--- src/Main.pas
+++ src/Main.pas
@@ -6,6 +6,7 @@ uses
   System.SysUtils,
   System.Classes,
   System.IOUtils,
+  System.JSON,
   System.Diagnostics;
 
 type
@@ -22,22 +23,61 @@ uses
   Command.AnalyseUnit,
   Command.AnalyseFolder;
 
-function GetTestFolder(): string;
+const
+  ConfigFileName = 'appconfig.json';
+
+type
+  TAppConfiguration = record
+    DataFolder: string;
+    TestFile: string;
+    TestFolder: string;
+  end;
+
+var
+  AppConfiguration: TAppConfiguration;
+
+function GetConfigValue(config: TJSONObject; const aKeyName: string): string;
+var
+  value: string;
 begin
-  if DirectoryExists('..\..\..\test\data') then
-    Result := '..\..\..\test\data\'
-  else if DirectoryExists('..\test\data') then
-    Result := '..\test\data'
+  if config.TryGetValue<string>('dataFolder', value) then
+    Result := value
   else
-    raise Exception.Create('Can''t find test data folder.');
+    raise EAssertionFailed.Create
+      (Format('Can''t find mandatory key in app config: %s', [aKeyName]));
+end;
+
+procedure ReadConfiguration();
+var
+  jsAppConfig: TJSONObject;
+begin
+  Assert(FileExists(ConfigFileName),
+    Format('Can''t run. Missing mandatory config file: %s', [ConfigFileName]));
+  jsAppConfig := TJSONObject.ParseJSONValue(TFile.ReadAllText(ConfigFileName))
+    as TJSONObject;
+  AppConfiguration.DataFolder := GetConfigValue(jsAppConfig, 'dataFolder');
+  AppConfiguration.TestFile := GetConfigValue(jsAppConfig, 'testFile');
+  AppConfiguration.TestFolder := GetConfigValue(jsAppConfig, 'testSubFolder');
+end;
+
+function GetTestFolder(): string;
+var
+  path2: string;
+begin
+  path2 := TPath.Combine('..\..\', AppConfiguration.DataFolder);
+  if DirectoryExists(AppConfiguration.DataFolder) then
+    Exit(AppConfiguration.DataFolder);
+  if DirectoryExists(path2) then
+    Exit(path2);
+  raise Exception.Create('Can''t find test data folder.');
 end;
 
 function IsDeveloperMode(): boolean;
 var
   dprFileName: string;
 begin
-  dprFileName :=  ChangeFileExt(ExtractFileName(ParamStr(0)),'.dpr');
-  Result := FileExists('..\..\'+dprFileName) or FileExists(dprFileName);
+  dprFileName := ChangeFileExt(ExtractFileName(ParamStr(0)), '.dpr');
+  Result := FileExists('..\..\' + dprFileName) or FileExists(dprFileName);
 end;
 
 function GetSampleFilePath(): string;
@@ -54,7 +94,7 @@ end;
 procedure RunAnalysisOnFolder();
 begin
   ConsoleApplicationHeader();
-  TAnalyseFolderCommand.Execute(GetTestFolder + 'mORMot');
+  TAnalyseFolderCommand.Execute(TPath.Combine(GetTestFolder, 'mORMot'));
 end;
 
 procedure RunAnalysisOnFile();
@@ -65,6 +105,7 @@ end;
 
 procedure ApplicationRun();
 begin
+  ReadConfiguration();
   case ApplicationMode of
     amFolderAnalysis:
       RunAnalysisOnFolder();
