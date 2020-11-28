6e33ff11e0e40db5d9c05df053011a51d35beec8
Bogdan Polak <bogdanpol@gmail.com>
2020-11-28 20:04:17 +0100

    Cleaned test01.pas unit

--- test/data/test01.pas
+++ test/data/test01.pas
@@ -2,39 +2,25 @@ unit test01;
 
 interface
 
-type
-  TDDDEmailValidation = class
-    procedure SetDefaultValuesIfVoid(
-      const aSenderEmail,aApplication,
-      aRedirectionURIPublicRoot,aRedirectionURISuccess: RawUTF8);
-  end;
+uses
+  System.SysUtils;
+
+procedure StoreMessage(const aTemplateFolder: string;
+  const TemplateFileName: string);
 
 implementation
 
-procedure TDDDEmailValidation.SetDefaultValuesIfVoid(
-  const aSenderEmail,aApplication,
-  aRedirectionURIPublicRoot,aRedirectionURISuccess: RawUTF8);
+procedure FileFromString(const aLines: string; const aFileName: string);
 begin
-  if Template.SenderEmail='' then
-    Template.SenderEmail := aSenderEmail;
-  if Template.Application='' then
-    Template.Application := aApplication;
-  if Template.FileName='' then
-    Template.FileName := 'EmailValidate.txt';
-  if (TemplateFolder='') and
-     not FileExists(string(Template.FileName)) then
-    FileFromString('Welcome to {{Application}}!'#13#10#13#10+
-      'You have registered as "{{Logon}}", using {{EMail}} as contact address.'#13#10#13#10+
-      'Please click on the following link to validate your email:'#13#10+
-      '{{ValidationUri}}'#13#10#13#10'Best regards from the clouds'#13#10#13#10+
-      '(please do not respond to this email)',
-      UTF8ToString(Template.FileName));
-  if Template.Subject='' then
-    Template.Subject := 'Please Validate Your Email';
-  if Redirection.RestServerPublicRootURI='' then
-    Redirection.RestServerPublicRootURI := aRedirectionURIPublicRoot;
-  if Redirection.SuccessRedirectURI='' then
-    Redirection.SuccessRedirectURI := aRedirectionURISuccess;
+end;
+
+procedure StoreMessage(const aTemplateFolder: string;
+  const TemplateFileName: string);
+begin
+  if (aTemplateFolder = '') and not FileExists(string(TemplateFileName)) then
+    FileFromString('Welcome to {{Application}}!' + sLineBreak + sLineBreak +
+      'Please click on the button:' + sLineBreak,
+      UTF8ToString(TemplateFileName));
 end;
 
 end.
