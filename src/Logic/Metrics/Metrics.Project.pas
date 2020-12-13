0e701c972f06b3a30cffee1c2eeee2d0966b6eab
Bogdan Polak <bogdanpol@gmail.com>
2020-12-13 23:03:25 +0100

    Added TUniMetrics collection to project

--- src/Logic/Metrics/Metrics.Project.pas
+++ src/Logic/Metrics/Metrics.Project.pas
@@ -5,18 +5,23 @@ interface
 uses
   System.Generics.Collections,
   {--}
-  Metrics.ClassM;
+  Metrics.ClassM,
+  Metrics.UnitM;
 
 type
   TProjectMetrics = class
   private
     fClasses: TObjectList<TClassMetrics>;
+    fUnits: TObjectList<TUnitMetrics>;
   public
     constructor Create();
     destructor Destroy; override;
     function ClassCount(): Integer;
     function GetClass(aIdx: Integer): TClassMetrics;
-    procedure AddClass(const aClassMetrics: TClassMetrics);
+    function AddClass(const aClassMetrics: TClassMetrics): TProjectMetrics;
+    function UnitCount(): Integer;
+    function GetUnit(aIdx: Integer): TUnitMetrics;
+    function AddUnit(const aUnitMetrics: TUnitMetrics): TProjectMetrics;
   end;
 
 implementation
@@ -24,16 +29,20 @@ implementation
 constructor TProjectMetrics.Create;
 begin
   fClasses := TObjectList<TClassMetrics>.Create();
+  fUnits := TObjectList<TUnitMetrics>.Create();
 end;
 
 destructor TProjectMetrics.Destroy;
 begin
+  fUnits.Free;
   fClasses.Free;
   inherited;
 end;
 
-procedure TProjectMetrics.AddClass(const aClassMetrics: TClassMetrics);
+function TProjectMetrics.AddClass(const aClassMetrics: TClassMetrics)
+  : TProjectMetrics;
 begin
+  Result := self;
   fClasses.Add(aClassMetrics);
 end;
 
@@ -45,6 +54,22 @@ end;
 function TProjectMetrics.GetClass(aIdx: Integer): TClassMetrics;
 begin
   Result := fClasses.Items[aIdx];
+end;
+
+function TProjectMetrics.AddUnit(const aUnitMetrics: TUnitMetrics): TProjectMetrics;
+begin
+  Result := self;
+  fUnits.Add(aUnitMetrics);
+end;
+
+function TProjectMetrics.GetUnit(aIdx: Integer): TUnitMetrics;
+begin
+  Result := fUnits[aIdx];
+end;
+
+function TProjectMetrics.UnitCount: Integer;
+begin
+  Result := fUnits.Count;
 end;
 
 end.
